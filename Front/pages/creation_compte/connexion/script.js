9330ceb4eef75ce86e682b3d9129d614f595de19
U-DESKTOP-VHE1VC8\Younes <yben213soin>
2025-04-01 10:08:06 +0200

    mvc

--- Front/pages/creation_compte/connexion/script.js
+++
@@ -1,8 +0,0 @@
-function togglePassword(inputId) {
-    let passwordInput = document.getElementById(inputId);
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-    } else {
-        passwordInput.type = "password";
-    }
-}
