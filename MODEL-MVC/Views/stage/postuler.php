11f94e448e34f648b7072cf9b4be1351dbcda3bb
Rayan <rayan.madjidrayain@gmail.com>
2025-04-08 08:21:18 +0200

    qsd

--- MODEL-MVC/Views/stage/postuler.php
+++ MODEL-MVC/Views/stage/postuler.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);i_set('display_startup_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);(E_ALL);
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/projetWEB/MODEL-MVC/Controllers/c_candidature.php'; // Inclusion du contrôleure_once $_SERVER['DOCUMENT_ROOT'] . '/projetWEB/MODEL-MVC/Controllers/c_candidature.php'; // Inclusion du contrôleur
