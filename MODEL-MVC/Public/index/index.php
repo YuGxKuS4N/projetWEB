d43b43716305b7ef8b1c52ca3e4c48c2c8e1954b
Rayan <rayan.madjidrayain@gmail.com>
2025-04-08 10:25:02 +0200

    Ajout d'un champ de mot de passe dans le formulaire de connexion et réorganisation de la navbar.

--- MODEL-MVC/Public/index/index.php
+++ MODEL-MVC/Public/index/index.php
@@ -15,17 +15,43 @@ include_once dirname(__DIR__) . '/Controllers/c_chargement.php';
 </head>  
 <body>  
     <header>  
-        <nav class="navbar">  
-            <div class="nav-logo">  
-                <a href="/projetWEB/MODEL-MVC/Public/index.php">  
-                     
-                </a>  
-            </div>  
-            <ul class="nav-right">  
-                <li><a href="/projetWEB/MODEL-MVC/Views/creation_compte/inscription.php">S'INSCRIRE</a></li>  
-                <li><a href="/projetWEB/MODEL-MVC/Views/creation_compte/connexion.php">CONNEXION</a></li>  
-            </ul>  
-        </nav>  
+    <nav class="navbar">
+        <ul class="nav-left">
+            <li><a href="/projetWEB/MODEL-MVC/Views/acceuil/acceuil.php">ACCUEIL</a></li>
+            <li><a href="/projetWEB/MODEL-MVC/Views/stage/stage.php">STAGE</a></li>
+        </ul>    
+
+        <div class="nav-logo">
+            <a href="/projetWEB/MODEL-MVC/Views/acceuil/acceuil.php">
+                <img src="/projetWEB/MODEL-MVC/Public/image/logo.png" alt="Logo du Site">
+            </a>
+        </div>
+
+        <uel class="nav-right">
+            <li><a href="/projetWEB/MODEL-MVC/Views/creation_compte/inscription.php">S'INSCRIRE</a></li>
+        </ul>
+    </nav>
+
+    <div class="form-box">
+        <form class="form" action="/projetWEB/MODEL-MVC/Controllers/c_connexion.php" method="POST">
+            <span class="title">Se connecter</span>
+            <span class="subtitle">Entrez vos identifiants pour accéder à votre compte.</span>
+            
+            <!-- Affichage des erreurs -->
+            <?php if (isset($_GET['error'])): ?>
+                <p class="error-message"><?= htmlspecialchars($_GET['error']) ?></p>
+            <?php endif; ?>
+
+            <div class="form-container">
+                <input type="email" class="input" name="email" placeholder="E-mail" required>
+                <input type="password" class="input" name="password" placeholder="Mot de passe" required>
+            </div>
+            <button type="submit">Connexion</button>
+        </form>
+        <div class="form-section">
+            <p>Pas encore de compte ? <a href="/projetWEB/MODEL-MVC/Views/creation_compte/inscription.php">Inscrivez-vous ici.</a></p>
+        </div>
+    </div>
     </header>  
 
     <div class="container" id="profile-container">  
