140d6f79ae0e5ceddabee1fbd7e61c92c7e370a6
Rayan <rayan.madjidrayain@gmail.com>
2025-04-11 00:08:03 +0200

    Correction de la requête SQL pour utiliser `id_offre` au lieu de `stage-id` dans le contrôleur de candidature

--- MODEL-MVC/Controllers/c_candidature.php
+++ MODEL-MVC/Controllers/c_candidature.php
@@ -46,7 +46,7 @@ class CandidatureController {
             INSERT INTO Candidature 
                 (id_etudiant_fk, id_offre_fk, date_candidature, statut_candidature, commentaire, id_entreprise_fk, cv_path, motivation_path)
             VALUES 
-                (?, ?, ?, 'en attente', NULL, (SELECT id_entreprise_fk FROM Offre_Stage WHERE `stage-id` = ?), ?, ?)
+                (?, ?, ?, 'en attente', NULL, (SELECT id_entreprise_fk FROM Offre_Stage WHERE id_offre = ?), ?, ?)
 SQL;
 
         $stmt = $this->conn->prepare($sql);
