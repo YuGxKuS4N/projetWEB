75dc656edcac23d12e291b0bfa7dae19e8eac7d7
Younes Bendani <toriguyounes@gmail.com>
2025-04-10 19:09:32 +0200

    message d erreur page d inscription

--- MODEL-MVC/Views/footer/footer.php
+++ MODEL-MVC/Views/footer/footer.php
@@ -1,17 +1,7 @@
 <!-- filepath: c:\wamp64\www\projetWEB\MODEL-MVC\Views\footer\footer.php -->
 <link rel="stylesheet" href="/projetWEB/MODEL-MVC/Public/css/footer.css"> <!-- Inclusion du CSS du footer -->
 <footer>
-    <div class="footer-content">
-        <h3>WEB4ALL</h3>
-        <p>WEB4ALL est une plateforme dédiée à la mise en relation entre entreprises, stagiaires et pilotes. Découvrez nos services et profitez d'une expérience unique.</p>
-    <ul class="socials">  
-        <li><a href="#"><img src="path/to/facebook-logo.png" alt="Facebook" width="20"></a></li>  
-        <li><a href="#"><img src="path/to/twitter-logo.png" alt="Twitter" width="20"></a></li>  
-        <li><a href="#"><img src="path/to/google-plus-logo.png" alt="Google+" width="20"></a></li>  
-        <li><a href="#"><img src="path/to/youtube-logo.png" alt="YouTube" width="20"></a></li>  
-        <li><a href="#"><img src="path/to/linkedin-logo.png" alt="LinkedIn" width="20"></a></li>  
-    </ul>  
-    </div>
+
     <div class="footer-bottom">
         <p>Copyright &copy; <?php echo date("Y"); ?> <a href="#">WEB4ALL</a>. Tous droits réservés.</p>
     </div>
