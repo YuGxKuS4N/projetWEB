20552c4a3909f9d8ec2f92b33a9abeb8a542878f
Younes Bendani <toriguyounes@gmail.com>
2025-04-10 19:01:48 +0200

    message d erreur page d inscription

--- MODEL-MVC/Views/footer/footer.php
+++ MODEL-MVC/Views/footer/footer.php
@@ -4,13 +4,13 @@
     <div class="footer-content">
         <h3>WEB4ALL</h3>
         <p>WEB4ALL est une plateforme dédiée à la mise en relation entre entreprises, stagiaires et pilotes. Découvrez nos services et profitez d'une expérience unique.</p>
-        <ul class="socials">
-            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-            <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
-        </ul>
+    <ul class="socials">  
+        <li><a href="#"><img src="path/to/facebook-logo.png" alt="Facebook" width="20"></a></li>  
+        <li><a href="#"><img src="path/to/twitter-logo.png" alt="Twitter" width="20"></a></li>  
+        <li><a href="#"><img src="path/to/google-plus-logo.png" alt="Google+" width="20"></a></li>  
+        <li><a href="#"><img src="path/to/youtube-logo.png" alt="YouTube" width="20"></a></li>  
+        <li><a href="#"><img src="path/to/linkedin-logo.png" alt="LinkedIn" width="20"></a></li>  
+    </ul>  
     </div>
     <div class="footer-bottom">
         <p>Copyright &copy; <?php echo date("Y"); ?> <a href="#">WEB4ALL</a>. Tous droits réservés.</p>
