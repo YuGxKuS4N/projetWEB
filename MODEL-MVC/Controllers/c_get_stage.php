6a4f0ff15e1d78ccf8451cefdeb14f13594b3a0f
Younes Bendani <toriguyounes@gmail.com>
2025-04-10 15:33:25 +0200

    message d erreur page d inscription

--- MODEL-MVC/Controllers/c_get_stage.php
+++ MODEL-MVC/Controllers/c_get_stage.php
@@ -10,8 +10,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);  
 error_reporting(E_ALL);  
 
-
 require_once __DIR__ . '/../Config/Database.php'; // Chemin corrigé
+
 class StageController {  
     private $db;  
     private $conn;  
@@ -157,4 +157,4 @@ try {
     http_response_code(500);  
     echo json_encode(["error" => "Erreur interne du serveur."]);  
 }  
-?>  
+?>
