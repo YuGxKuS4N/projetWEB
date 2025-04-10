005912e20aa811079ecd6f34278e1634aa324734
Younes Bendani <toriguyounes@gmail.com>
2025-04-10 17:48:17 +0200

    message d erreur page d inscription

--- MODEL-MVC/Public/index/index.php
+++ MODEL-MVC/Public/index/index.php
@@ -16,7 +16,7 @@ $logoutMessage = isset($_GET['logout']) && $_GET['logout'] === 'success' ? "Vous
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
