8daa723cc505e3aee166ef17f8fc331bc758fffe
Younes Bendani <toriguyounes@gmail.com>
2025-04-11 19:04:10 +0200

    page profil

--- MODEL-MVC/Controllers/c_get_data.php
+++ MODEL-MVC/Controllers/c_get_data.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-require_once __DIR__ . '/c_connexion.php';
-require_once __DIR__ . '/../Config/config.php';
-require_once __DIR__ . '/../Config/Database.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/projetWEB/MODEL-MVC/Controllers/c_connexion.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/projetWEB/MODEL-MVC/Config/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/projetWEB/MODEL-MVC/Config/Database.php';
 
 // Vérifiez si l'utilisateur est connecté via la session
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['role'])) {
