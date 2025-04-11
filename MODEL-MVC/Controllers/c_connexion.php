8a18d7afcc597990cdb79d8d57d120ac5d9aa77f
Younes Bendani <toriguyounes@gmail.com>
2025-04-11 19:28:33 +0200

    page profil

--- MODEL-MVC/Controllers/c_connexion.php
+++ MODEL-MVC/Controllers/c_connexion.php
@@ -1,5 +1,7 @@
 <?php
-
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 /**
  * Contrôleur pour gérer la connexion des utilisateurs.
  * 
--- MODEL-MVC/Controllers/c_get_data.php
+++ MODEL-MVC/Controllers/c_get_data.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 require_once __DIR__ . '/c_connexion.php'; // Inclusion du fichier connexion
 require_once __DIR__ . '/../Config/config.php';
 require_once __DIR__ . '/../Config/Database.php';
