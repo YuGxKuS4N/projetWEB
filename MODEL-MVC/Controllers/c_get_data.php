cb00026a75ff6b5e65d86f4a5ca0f661c3c7523a
Younes Bendani <toriguyounes@gmail.com>
2025-04-11 18:43:48 +0200

    profil

--- MODEL-MVC/Controllers/c_get_data.php
+++ MODEL-MVC/Controllers/c_get_data.php
@@ -4,14 +4,29 @@ require_once __DIR__ . '/../Config/config.php';
 require_once __DIR__ . '/../Config/Database.php';
 
 // Vérifiez si l'utilisateur est connecté via la session
-session_start();
+
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['role'])) {
     error_log("Utilisateur non connecté.");
     header('Content-Type: application/json');
     echo json_encode(["error" => "Utilisateur non connecté."]);
     exit();
 }
+// Activer l'affichage des erreurs
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+// Vérifier si la session est déjà active avant d'appeler session_start()
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Vérifier si l'utilisateur est connecté
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['role'])) {
+    // Rediriger vers la page de connexion si non connecté
+    header("Location: ../creation_compte/connexion.php");
+    exit();
+}
 // Récupérez les informations de l'utilisateur connecté
 $userId = $_SESSION['user_id'];
 $userType = $_SESSION['role'];
